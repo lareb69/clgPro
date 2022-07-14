d73f5bdf3ca71bc1d18d4e0c7cf2d2233466295d
orient69 <orientsplash@gmail.com>
2022-07-14 22:22:46 +0530

    designe admin page

--- client/src/admin/Compose.jsx
+++ client/src/admin/Compose.jsx
@@ -1,10 +1,10 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React, { useState } from 'react';
 import qs from 'qs';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
 const Compose = () => {
-    const navigate = useNavigate();
     const [inputData, setInputData] = useState('');
     const [question, setQuestion] = useState({});
 
@@ -20,7 +20,7 @@ const Compose = () => {
     };
 
     return (
-        <div className="flex item-center justify-center flex-col p-8 w-full">
+        <div className="flex justify-center items-center flex-col  w-screen  sm:w-[50vw] mx-auto mt-8">
             <form
                 onSubmit={(e) => {
                     e.preventDefault();
@@ -34,15 +34,33 @@ const Compose = () => {
                         });
                 }}
             >
-                <input
+                <label
+                    htmlFor="question-input"
+                    className="text-gray-800 text-lg font-bold py-2 inline-block"
+                >
+                    Question
+                </label>
+                <textarea
                     onChange={(e) => {
                         setInputData(e.target.value);
                     }}
                     type="text"
                     value={inputData}
-                    className="border rounded min-w-full sm:w-80"
+                    className="w-full sm:w-[50vw] px-2 py-1 bg-white transition ease-in-out  font-normal text-gray-700 bg-clip-padding border border-solid border-gray-300 rounded focus:bg-white focus:text-gray-800 focus:border-blue-600 focus:outline-none"
+                    id="question-input"
+                    rows={3}
                 />
-                <button className="px-8 py-4">Submit</button>
+                <label htmlFor="answer-input">Answer</label>
+                <textarea
+                    type="text"
+                    className="w-full sm:w-[50vw] px-2 py-1 bg-white transition ease-in-out  font-normal text-gray-700 bg-clip-padding border border-solid border-gray-300 rounded focus:bg-white focus:text-gray-800 focus:border-blue-600 focus:outline-none"
+                    id="answer-input"
+                    rows={10}
+                />
+
+                <button className="px-4 mt-4 sm:text-lg bg-slate-700 font-normal py-2  rounded text-white/95 transition-colors ease-in hover:bg-slate-600 hover:text-white">
+                    Submit
+                </button>
             </form>
         </div>
     );
